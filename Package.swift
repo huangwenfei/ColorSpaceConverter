e9f188b2ce7bd2390c877727f223c4da64db8495
huangwenfei <yi.yuan.zi@163.com>
2024-12-25 17:30:41 +0800

    修改 package

--- Package.swift
+++ Package.swift
@@ -41,6 +41,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ColorSpaceConverter",
+            type: .dynamic,
             targets: ["ColorSpaceConverter"]
         ),
     ],
